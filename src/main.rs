a2a6c93dcd3373eeccaae986749ad5dc4547d1f1
razzdrgn <razzdrgn@protonmail.com>
2023-11-15 21:39:15 -0500

    run rustfmt

--- src/main.rs
+++ src/main.rs
@@ -10,16 +10,15 @@ pub struct Data {}
 
 // Shuttle Service struct, each field represents a "runtime" (ar at least that's how I'm thinking of it)
 pub struct RazzbotService {
-	discord: Arc<poise::Framework<Data, Box<(dyn std::error::Error + std::marker::Send + Sync + 'static)>>>,
+	discord: Arc<
+		poise::Framework<Data, Box<(dyn std::error::Error + std::marker::Send + Sync + 'static)>>,
+	>,
 }
 
 // Defines the function that processes all the startup details for each runtime in the service
 #[shuttle_runtime::async_trait]
 impl shuttle_runtime::Service for RazzbotService {
-	async fn bind(
-		mut self,
-		addr: std::net::SocketAddr,
-	) -> Result<(), shuttle_runtime::Error> {
+	async fn bind(mut self, addr: std::net::SocketAddr) -> Result<(), shuttle_runtime::Error> {
 		tokio::select!(
 			_ = self.discord.start() => {},
 		);
@@ -33,7 +32,9 @@ async fn razzbot(
 	#[shuttle_secrets::Secrets] secrets: SecretStore,
 ) -> Result<RazzbotService, shuttle_runtime::Error> {
 	// Get the token from the SecretStore
-	let token = secrets.get("DISCORD_TOKEN").context("'DISCORD_TOKEN' not found")?;
+	let token = secrets
+		.get("DISCORD_TOKEN")
+		.context("'DISCORD_TOKEN' not found")?;
 
 	// Build the Poise Framework to use as the Discord runtime
 	let discord_client = poise::Framework::builder()
@@ -43,7 +44,8 @@ async fn razzbot(
 		})
 		.token(token)
 		.intents(serenity::GatewayIntents::non_privileged()) // Discord Gateway Intents go in here using or (|) as bitwise combiners
-		.setup(|ctx, _ready, framework| { // I'll be real I'm not sure what this is doing
+		.setup(|ctx, _ready, framework| {
+			// I'll be real I'm not sure what this is doing
 			Box::pin(async move {
 				poise::builtins::register_globally(ctx, &framework.options().commands).await?;
 				Ok(Data {})
